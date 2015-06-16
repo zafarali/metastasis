ce6b89b70bd24bac851f5ec0a864b5b700bc524c
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2015-06-16 10:51:54 -0400

    Fix #11 can now plot the tumor in 2d and 3d with tags and locations visible

--- cc3dtools/SpacePlot.py
+++ cc3dtools/SpacePlot.py
@@ -1,10 +1,11 @@
 ## Author @zafarali
 ## June 2015
 
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import csv
 
-def spatial_plot( start_file = None, end_file = None, type_colors = ( 'r', 'b', 'g' ), format = ( 'id' , 'type' , 'x', 'y', 'z' ) ):
+def spatial_plot( start_file = None , end_file = None , type_colors = ( 'r', 'b', 'g' ) , format = ( 'id' , 'type' , 'x', 'y', 'z' ) , projection = '2d' ):
 	assert start_file is not None and end_file is not None, 'must specify files'
 
 	points = []
@@ -15,7 +16,11 @@ def spatial_plot( start_file = None, end_file = None, type_colors = ( 'r', 'b', 
 		for row in reader:
 			starter_cells.append(  int( row[0] )  )
 
-	plt.figure()
+
+	fig = plt.figure()
+
+	if projection == '3d':
+		ax = fig.gca( projection = '3d' )
 
 	with open( end_file , 'r' ) as f:
 		reader = csv.reader( f )
@@ -30,9 +35,25 @@ def spatial_plot( start_file = None, end_file = None, type_colors = ( 'r', 'b', 
 			data['z'] = float( data['z'] )
 
 			data['initial'] = 1 if data['id'] in starter_cells else 0
-			print data
 
-			plt.text( data['x'] , data['y'] , data['id'] , horizontalalignment = 'center', color = type_colors[ data['type'] - 1 ] )
+			marker = 'x' if data['initial'] else 'o'
+			if projection == '3d':
+				ax.scatter( data['x'] , data['y'] , data['z'] , marker = marker , color = type_colors[ data['type'] - 1 ] )
+				ax.text( data['x']+0.5 , data['y']+0.5 , data['z'] , str( data['id'] ) , horizontalalignment = 'center' , color = type_colors[ data['type'] - 1 ] )
+			else:
+				plt.plot( data['x'] , data['y'] , marker+type_colors[ data['type'] - 1 ] )
+				plt.text( data['x']+0.5 , data['y']+0.5 , str( data['id'] ) , horizontalalignment = 'center' , color = type_colors[ data['type'] - 1 ] )
+
+	if projection == '3d':
+		ax.set_xlabel( 'x-axis' )
+		ax.set_ylabel( 'y-axis' )
+		ax.set_zlabel( 'z-axis' )
+		ax.set_title('Locations of Genomes at time of final sampling', 'center')
+	else:
+		plt.xlabel('x-axis')
+		plt.ylabel('y-axis')
+		plt.title('Locations of Genomes at time of final sampling')
+
 
 	plt.show()
 
