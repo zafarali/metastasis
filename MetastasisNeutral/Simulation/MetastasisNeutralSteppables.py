8ee2a2050d089b9254823bb32d8fe88148efb8a8
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2015-07-07 10:44:21 -0400

    Initial commit for neutral model #27

---
+++ MetastasisNeutral/Simulation/MetastasisNeutralSteppables.py
@@ -0,0 +1,157 @@
+
+from PySteppables import *
+import CompuCell
+import sys
+import CompuCellSetup
+import numpy as np
+
+from PySteppablesExamples import MitosisSteppableBase
+
+
+save_flag = True
+simulate_flag = True
+
+divide_times = {'last_division':0}
+GLOBAL = {
+    'targetVolume':50,
+    'divideThreshold':65,
+    'cancer2_divideThreshold':65,
+    'cancer1_divideThreshold':65,
+    'maxTargetVolume':75,
+    'cancer2_additional_dV':0,
+    'cancer1_additional_dV':0,
+    'dV':0.05
+}
+
+import time 
+time_info = '_'.join(time.asctime().split(' '))
+
+
+from cc3dtools.Tracker import Tracker2
+from cc3dtools.Genome import Genome, save_genomes
+genomes = {}
+
+class ConstraintInitializerSteppable(SteppableBasePy):
+    def __init__(self,_simulator,_frequency=1):
+        SteppableBasePy.__init__(self,_simulator,_frequency)
+        if save_flag:
+            self.start_tracker = Tracker2( file_name = '../start_cells_' + time_info + '.csv')
+
+    def start(self):
+        
+        num_cells = len(self.cellList)
+
+
+        for cell in self.cellList:
+            
+            divide_times[cell.id] = 0
+            
+            cell.targetVolume=GLOBAL['targetVolume']
+            cell.lambdaVolume=1.5
+
+            if simulate_flag:
+                genomes[cell.id] = Genome( mutation_rate = 50 , name = cell.id )
+
+            if save_flag:
+                self.start_tracker.stash( [ cell.id, cell.type , genomes[cell.id].mutation_rate ] )
+            # holder[cell.id] = { 'g': Genome( mutation_rate = 20 , genome_order = 10 ) }
+
+
+
+    def finish(self):
+        if save_flag:
+            tracker = Tracker2( file_name = '../finish_cells_' + time_info + '.csv' )
+
+            for cell in self.cellList:
+                z = cell.zCOM
+                y = cell.yCOM
+                x = cell.xCOM
+                tracker.stash( [ cell.id , cell.type , x , y , z ] )
+
+            tracker.save_stash() # save final cell data
+            self.start_tracker.save_stash() # save initial cell data
+            save_genomes( [ genome[1] for genome in genomes.items() ] , file_name = '../genomes_' + time_info + '.csv' ) #save genomes
+
+        
+
+        
+
+class GrowthSteppable(SteppableBasePy):
+    def __init__(self,_simulator,_frequency=1):
+        SteppableBasePy.__init__(self,_simulator,_frequency)
+    def step(self,mcs):
+        #                   print '----->Global targetVolume',GLOBAL['targetVolume']
+        for cell in self.cellList:
+            # don't grow if your volume is greater than the maximumTargetVolume
+
+            cell.targetVolume = min( GLOBAL['dV'] + cell.targetVolume , GLOBAL['maxTargetVolume'] )
+            
+
+
+
+class MitosisSteppable(MitosisSteppableBase):
+    def __init__(self,_simulator,_frequency=1):
+        MitosisSteppableBase.__init__(self,_simulator, _frequency)
+        if save_flag:
+            self.mitosis_tracker = Tracker2(file_name='../division_events_' + time_info + '.csv')
+
+    def start(self):
+        # we initialize the stash function
+        if save_flag:
+            for cell in self.cellList:
+                                        # R for 'root'
+                self.mitosis_tracker.stash( [ 'R' , cell.id, cell.id, cell.id ] )
+
+
+    def step(self,mcs):
+        # print "INSIDE MITOSIS STEPPABLE"
+        cells_to_divide=[]
+        for cell in self.cellList:
+            if ( cell.type == self.CANCER2 and cell.volume > GLOBAL['cancer2_divideThreshold'] ) or \
+            ( cell.type == self.CANCER1 and cell.volume > GLOBAL['cancer1_divideThreshold'] ) or \
+            cell.volume > GLOBAL['divideThreshold'] :
+            # if cell.volume > 100:
+                cells_to_divide.append(cell)
+                
+        for cell in cells_to_divide:
+            # to change mitosis mode leave one of the below lines uncommented
+            self.divideCellRandomOrientation(cell)
+            divide_times['last_division'] = mcs
+
+
+    def updateAttributes(self):
+        parentCell=self.mitosisSteppable.parentCell
+        childCell=self.mitosisSteppable.childCell
+
+        divide_times[parentCell.id] = divide_times['last_division']
+        divide_times[childCell.id] = divide_times['last_division']
+
+        ## using the equation derived to keep force in the model constant
+        ## T_star = T_target - t/2 <-- t/2 is volume after division 
+        T_star = GLOBAL['targetVolume'] - ( parentCell.volume / 2.0 )
+
+
+        childCell.targetVolume = T_star
+        parentCell.targetVolume = T_star
+
+
+        if simulate_flag:
+            genomes[parentCell.id].mutate()
+            genomes[childCell.id] = genomes[parentCell.id].replicate( name = childCell.id )
+
+        childCell.lambdaVolume = 1.5
+        parentCell.lambdaVolume = 1.5
+
+
+        childCell.type = parentCell.type
+
+
+
+        if save_flag:
+            self.mitosis_tracker.stash( [ divide_times['last_division'] , parentCell.id, childCell.id, parentCell.id ] )
+
+    def finish(self):
+        if save_flag:
+            self.mitosis_tracker.save_stash()
+        pass
+
