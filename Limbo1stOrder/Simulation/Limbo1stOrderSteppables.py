13125d82d78a73b4f8403aab499a1c6ff797ea7e
zafarali <zafarali.ahmed@gmail.com>
2015-08-04 16:31:07 -0400

    turn off premature saving temporarily to see what results look like #66

--- Limbo1stOrder/Simulation/Limbo1stOrderSteppables.py
+++ Limbo1stOrder/Simulation/Limbo1stOrderSteppables.py
@@ -390,7 +390,8 @@ class DeathCheckSteppable(SteppableBasePy):
 
             if not ( ( x >= LATTICE['x_min'] and x <= LATTICE['x_max'] ) and ( y >= LATTICE['y_min'] and y <=LATTICE['y_max'] ) ) :
                 if cell.type == self.CANCER1 or cell.type == self.CANCER2:
-                    self.stopSimulation()
+                    # self.stopSimulation()
+                    pass
                 # cell.lambdaVolume = 5
                 cell.type = self.DEAD
                 self.lambdaVolume = 1
