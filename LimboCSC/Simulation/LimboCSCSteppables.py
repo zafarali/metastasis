fa780d8576071677948924bf52248efc41610c03
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2015-11-04 18:09:52 -0500

    fix divisions_left issue

--- LimboCSC/Simulation/LimboCSCSteppables.py
+++ LimboCSC/Simulation/LimboCSCSteppables.py
@@ -252,9 +252,10 @@ class MitosisSteppable(MitosisSteppableBase):
                 continue
 
 
-            if ( cell.type == self.CANCER2 and cell.volume > GLOBAL['cancer2_divideThreshold'] ) or \
+            if ( ( cell.type == self.CANCER2 and cell.volume > GLOBAL['cancer2_divideThreshold'] ) or \
             ( cell.type == self.CANCER1 and cell.volume > GLOBAL['cancer1_divideThreshold'] ) or \
-            cell.volume > GLOBAL['divideThreshold'] :
+            cell.volume > GLOBAL['divideThreshold'] and \
+            divisions_left[cell.id] != 0 ):
             # if cell.volume > 100:
                 cells_to_divide.append(cell)
                 
