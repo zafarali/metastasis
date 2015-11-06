b79d01fb6924cb52868a4d7374b17ceb1b47c39f
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2015-11-06 11:00:22 -0500

    New spaceless model

---
+++ spaceless/Model.py
@@ -0,0 +1,15 @@
+
+class Genome(object):
+	def __init__(self):
+		pass
+
+class Phenotypes(object):
+	def __init__(self):
+		pass
+
+class Cell(object):
+	def __init__(self):
+		pass
+
+class Simulator(object):
+	
